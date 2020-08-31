2fd8c1d7ec0152be1cd7736874a1f8385c57e9a0
Veronika Romashkina <vrom911@gmail.com>
2020-08-31 12:16:34 +0100

    WIP

--- src/GithubGraphql.hs
+++ src/GithubGraphql.hs
@@ -13,3 +13,54 @@ module GithubGraphql
 
 projectName :: String
 projectName = "github-graphql"
+
+myQuery :: GraphQLQuery
+myQuery = toGraphQL $
+    repository
+        & owner =. "kowainik"
+        & name =. "stan"
+        >. pullRequests
+            & last =. 5
+            & states =. [Closed, Open]
+            >. nodes
+               ?. title
+               >. author
+                   ?. login
+
+newtype GraphQLQuery = GraphQLQuery Text
+
+class ToGraphQL a where
+    toGraphQL :: a -> GraphQLQuery
+
+data Repository = Repository
+    { rOwner :: !Text
+    , rName  :: !Text
+    , rQuery :: !(Map RepoNode Connection)
+    }
+
+instance ToGraphQL Repositry where
+    toGraphQL Repository{..} = GrqphQLQuery $
+      "repository(" <> "owner: " <> rOwner <> ", name: " <> rName <> ") {" <>
+      <> toGraphQL rQuery
+      <> "}"
+
+data RepoNode
+    = PullRequests
+    | Issues
+
+data Connection a = Connection
+    { last     :: !Int
+    , states   :: ![State]
+    , nodes    :: ![a]
+    , edges    :: ![Edge a]
+    , pageInfo :: !()
+    }
+
+data Edge a = Edge
+    { edgeCursor :: !Text
+    , edgeNode   :: !a
+    }
+
+data State
+    = Open
+    | Closed
