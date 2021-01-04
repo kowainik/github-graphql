f5ee3f5cb6e54868788a178766fbbd8c4f95b200
Veronika Romashkina <vrom911@gmail.com>
2021-01-04 14:52:51 +0000

    [#11] Split AST types into different modules
    Resolves #11

---
+++ src/GitHub/Author.hs
@@ -0,0 +1,67 @@
+{- |
+Copyright: (c) 2021 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Data types and helper functions to work with @author@/@actor@.
+-}
+
+module GitHub.Author
+    ( -- * Data types
+      AuthorField (..)
+    , HasAuthor (..)
+
+      -- * Smart constructors
+    , login
+
+      -- * AST functions
+    , authorToAst
+    , authorFieldToAst
+
+    ) where
+
+import Data.List.NonEmpty (NonEmpty (..))
+
+import GitHub.GraphQL (NodeName (..), QueryNode (..), mkQuery, nameNode)
+import GitHub.Issues (IssueField (..))
+import GitHub.PullRequests (PullRequestField (..))
+
+
+{- | Fields of the @Actor@ object.
+
+* https://developer.github.com/v4/interface/actor/
+-}
+-- TODO: rename data type to 'ActorField'?
+data AuthorField
+    = AuthorLogin
+    | AuthorResourcePath
+    | AuthorUrl
+
+authorToAst :: NonEmpty AuthorField -> QueryNode
+authorToAst authorFields = QueryNode
+    { queryNodeName = NodeAuthor
+    , queryNodeArgs = []
+    , queryNode     = mkQuery authorFieldToAst authorFields
+    }
+
+authorFieldToAst :: AuthorField -> QueryNode
+authorFieldToAst = \case
+    AuthorLogin        -> nameNode NodeLogin
+    AuthorResourcePath -> nameNode NodeResourcePath
+    AuthorUrl          -> nameNode NodeUrl
+
+login :: AuthorField
+login = AuthorLogin
+
+{- | Typeclass for objects that have an @author@ field.
+-}
+class HasAuthor field where
+    author :: NonEmpty AuthorField -> field
+
+instance HasAuthor IssueField where
+    author :: NonEmpty AuthorField -> IssueField
+    author = IssueAuthor
+
+instance HasAuthor PullRequestField where
+    author :: NonEmpty AuthorField -> PullRequestField
+    author = PullRequestAuthor
---
+++ src/GitHub/Connection.hs
@@ -0,0 +1,45 @@
+{- |
+Copyright: (c) 2021 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Data types and helper functions to work with connections and nodes.
+-}
+
+module GitHub.Connection
+    ( -- * Data types
+      Connection (..)
+      -- * Smart constructors
+    , nodes
+      -- * AST functions
+    , connectionToAst
+    ) where
+
+import Data.List.NonEmpty (NonEmpty (..))
+
+import GitHub.GraphQL (NodeName (..), QueryNode (..), mkQuery, nameNode)
+
+
+{- | Generic type for connections since they share the same
+fields. Examples are:
+
+* IssueConnection: https://developer.github.com/v4/object/issueconnection/
+* PullRequestConnection: https://developer.github.com/v4/object/pullrequestconnection/
+-}
+data Connection fld
+    = Nodes !(NonEmpty fld)
+    | Edges
+
+connectionToAst :: (fld -> QueryNode) -> Connection fld -> QueryNode
+connectionToAst fldToAst = \case
+    Nodes fields -> QueryNode
+        { queryNodeName = NodeNodes
+        , queryNodeArgs = []
+        , queryNode = mkQuery fldToAst fields
+        }
+    Edges -> nameNode NodeEdges
+
+{- | Smart constructor for the 'Nodes' 'Connection'.
+-}
+nodes :: NonEmpty fld -> Connection fld
+nodes = Nodes
